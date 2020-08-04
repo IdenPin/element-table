80468f291b1e2e81424eea975727c95d49fc88cf
pdeng <635639654@qq.com>
2020-08-04 16:29:27 +0800

    fix: 解决umd 引入错误问题

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
   // 修改 src 为 examples
   pages: {
@@ -6,5 +7,18 @@ module.exports = {
       template: 'public/index.html',
       filename: 'index.html'
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include
+      .add(path.join(__dirname, '/packages'))
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        // 修改它的选项...
+        return options
+      })
   }
 }
