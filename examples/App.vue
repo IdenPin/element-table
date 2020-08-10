4ad897f0d157c033cd9a18dc2633d762da2851c1
pdeng <635639654@qq.com>
2020-08-11 00:24:38 +0800

    docs: modify main entry

--- examples/App.vue
+++ examples/App.vue
@@ -26,7 +26,7 @@ export default {
   name: 'App',
   data () {
     return {
-      activeName: '3'
+      activeName: '1'
     }
   },
   components: {
--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import ElTableBlock from '../packages/index'
+// import ElTableBlock from '@pdeng/element-table/lib/elTableBlock.umd'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
