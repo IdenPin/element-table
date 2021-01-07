61cbd8fd5a6d3c69c752874fb524fc12adba8923
pdeng <635639654@qq.com>
2021-01-07 12:00:42 +0800

    feat: 发布npm

--- packages/index.js
+++ packages/index.js
@@ -3,18 +3,14 @@ import Column from './column'
 import HRender from './h-render'
 
 // 存储组件列表
-const components = [
-  ElTableBlock,
-  Column,
-  HRender
-]
+const components = [ElTableBlock, Column, HRender]
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = function (Vue) {
   // 判断是否安装
   if (install.installed) return
   // 遍历注册全局组件
-  components.map(component => Vue.component(component.name, component))
+  components.map((component) => Vue.component(component.name, component))
 }
 
 // 判断是否是直接引入文件
